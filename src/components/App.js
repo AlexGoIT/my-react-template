dfce9046dd09f783b5e2c709d933b5a41419daab
AlexGoIT <alexandrpuryaev@gmail.com>
2023-08-01 22:27:05 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
-import { Component } from "react";
-import "./App.css";
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <div className="App">Learn React</div>;
+    return <></>;
   }
 }
